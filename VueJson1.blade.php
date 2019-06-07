661e5676d96b01f353bac143ac98e3721b2f9f58
ThGzLT <Thgzlt@gmail.com>
2019-06-07 15:26:27 +0300

    Update VueJson1.blade.php

--- VueJson1.blade.php
+++ VueJson1.blade.php
@@ -1,28 +1,23 @@
 @extends('layouts.main')
 @section('content')
 
-<div id="app1">
-    <h1>Bitcoin Price Index</h1>
 
-    <section v-if="errored">
-      <p>We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
-    </section>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.17/vue.js"></script>
 
-    <section v-else>
-      <div v-if="loading">Loading...</div>
+<div id="app1" v-cloak>
+ <input v-model="term" type="search">
+ <button @click="search">Search</button>
+ <p/>
+    <div v-for="post in posts" class="post">
+      <p><strong>@{{post.title}}</strong></p>
+      <p>@{{post.body}}</p
+      <br clear="left">
+    {{--  <input v-model="post">  --}}
 
-      <div
-        v-else
-        v-for="currency in info"
-        class="currency"
-      >
-        @{{ currency.description }}:
-        <span class="lighten">
-          <span v-html="currency.symbol"></span>@{{ currency.rate_float | currencydecimal }}
-        </span>
-      </div>
+<td>@{{post}}</td>
 
-    </section>
-  </div>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.10/vue.js"></script>
+
+
+
   @endsection
+
